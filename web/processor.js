616fa4661cdb462fcb76c1f5e75ac30d2023b51a
croissantderp <bookish.gecko@gmail.com>
2022-08-12 23:06:35 -0400

    downloadable csv

--- web/processor.js
+++ web/processor.js
@@ -86,5 +86,25 @@ function initiate() {
 
 function end() {
     document.getElementById("results").innerHTML = finalString;
+    download(finalString.replaceAll("<br />", "\n"), "output.csv", "text/plain");
 }
 
+//from https://stackoverflow.com/questions/13405129/create-and-save-a-file-with-javascript
+// Function to download data to a file
+function download(data, filename, type) {
+    var file = new Blob([data], { type: type });
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+            url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function () {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+    }
+}
